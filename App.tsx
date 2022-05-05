0b6bbc965030a6d29ff06c49dcede63d527ca511
Armando Joergensen <armando-a-j@hotmail.com>
2022-05-05 20:13:03 -0300

    chore: Update theme provider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 import AppLoading from "expo-app-loading";
+
 import {
   Montserrat_600SemiBold,
   Montserrat_700Bold,
@@ -10,12 +10,16 @@ import {
   useFonts,
   WorkSans_700Bold,
   WorkSans_600SemiBold,
+  WorkSans_500Medium,
 } from "@expo-google-fonts/work-sans";
+
+import theme from "@/theme";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     WorkSans_700Bold,
     WorkSans_600SemiBold,
+    WorkSans_500Medium,
     Montserrat_600SemiBold,
     Montserrat_700Bold,
   });
@@ -23,19 +27,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/theme/index.ts
@@ -0,0 +1,21 @@
+export default {
+  COLORS: {
+    BACKGROUND: "#E5E5E5",
+
+    DEFAULT: "#000000",
+
+    PRIMARY_700: "#504DB6",
+    PRIMARY_500: "#8775FE",
+
+    SECONDARY_700: "#6F6F6F",
+    SECONDARY_500: "#85868A",
+    SECONDARY_400: "#CCCCCC",
+  },
+
+  FONTS: {
+    PRIMARY_TITLE: "WorkSans_700Bold",
+    SECONDARY_TITLE: "WorkSans_600Bold",
+    DESCRIPTION: "WorkSans_500Medium",
+  },
+};
+
