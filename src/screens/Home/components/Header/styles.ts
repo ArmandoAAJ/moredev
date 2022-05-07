057170e81ee2a614fde0fa8b9f3706a26e9ce697
Armando Joergensen <armando-a-j@hotmail.com>
2022-05-07 18:23:06 -0300

    improvement: update color by theme

--- src/screens/Home/components/Header/styles.ts
+++ src/screens/Home/components/Header/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  height: 21.5px;
   margin: 0 18px;
   align-items: center;
 `;
