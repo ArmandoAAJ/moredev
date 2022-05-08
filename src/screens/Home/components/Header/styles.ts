3318a3cbc8834544b8e2c3daeca7761860fa091c
Armando Joergensen <armando-a-j@hotmail.com>
2022-05-08 00:35:06 -0300

    feat: badge count products in cart

--- src/screens/Home/components/Header/styles.ts
+++ src/screens/Home/components/Header/styles.ts
@@ -6,3 +6,18 @@ export const Container = styled.View`
   margin: 0 18px;
   align-items: center;
 `;
+
+export const Content = styled.Pressable``;
+
+export const Badge = styled.View`
+  width: 14px;
+  height: 14px;
+  border-radius: 7px;
+  background-color: ${({ theme: { COLORS } }) => COLORS.HOME_BADGE};
+  position: absolute;
+  z-index: 1;
+  top: -5px;
+  right: -5px;
+  align-items: center;
+  justify-content: center;
+`;
