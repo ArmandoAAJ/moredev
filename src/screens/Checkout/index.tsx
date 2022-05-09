11359642c0f4f0e9e7865a0470bf96d6212022cd
Armando Joergensen <armando-a-j@hotmail.com>
2022-05-08 22:42:41 -0300

    feat: checkout screen

--- src/screens/Checkout/index.tsx
+++ src/screens/Checkout/index.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
-
-import { Container } from './styles';
+import React, { useMemo, useState } from "react";
+import Header from "./components/Header";
+import { DefaultContainer } from "@/shared/Commom/styles";
+import { useNavigation, CommonActions } from "@react-navigation/native";
+import ListCart from "./components/ListCart";
+import { useCart } from "@/hooks/Cart";
+import { ProductType } from "@/hooks/Products/types";
+import { Footer } from "./components/Footer";
+import { StatusBar } from "expo-status-bar";
+import { useTheme } from "styled-components";
 
 const Checkout: React.FC = () => {
-  return <View />;
-}
+  const { COLORS } = useTheme();
+  const { addItemToCart, removeItemToCart } = useCart();
+  const navigation = useNavigation();
+  const { cart, setCart } = useCart();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  function handleAddproductToCart(product: ProductType) {
+    addItemToCart(product);
+  }
+
+  function handleRemoveproductToCart(product: ProductType) {
+    removeItemToCart(product);
+  }
+
+  function handleSuccessCheckout() {
+    setCart([]);
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 1,
+        routes: [{ name: "success" }],
+      })
+    );
+  }
+
+  const sum = useMemo(() => {
+    const total = cart.reduce((acc, p) => acc + p.price * p.quantity!, 0);
+    return total;
+  }, [cart]);
+
+  return (
+    <>
+      <StatusBar backgroundColor={COLORS.BACKGROUND} style="dark" />
+      <DefaultContainer>
+        <Header onPress={handleGoBack} />
+        <ListCart
+          cart={cart}
+          addProductToCart={(product) => handleAddproductToCart(product)}
+          removeProductToCart={(product) => handleRemoveproductToCart(product)}
+        />
+        <Footer sum={sum} confirmCheckout={handleSuccessCheckout} />
+      </DefaultContainer>
+    </>
+  );
+};
 
 export default Checkout;
