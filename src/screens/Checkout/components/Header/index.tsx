e5be0cc795ccf5468b6344f6fd3320404f206c44
Armando Joergensen <armando-a-j@hotmail.com>
2022-05-08 22:24:02 -0300

    feat: header component for checkout

---
+++ src/screens/Checkout/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Pressable, View } from "react-native";
+import Svg, { Path } from "react-native-svg";
+import { useTheme } from "styled-components";
+import { Typograph } from "@/shared/Typograph";
+
+import { Back, Container } from "./styles";
+import { Separator } from "@/shared/Separator";
+import { Props } from "./types";
+
+const Header = ({ onPress }: Props) => {
+  const { COLORS } = useTheme();
+  return (
+    <>
+      <Separator height={11} />
+      <Container>
+        <Back onPressIn={() => onPress()}>
+          <Svg width={10} height={11} fill="none">
+            <Path
+              d="m7.103 10.012-5-5 5-5 1.125 1.125L4.36 5.012l3.867 3.875-1.125 1.125Z"
+              fill="#85868A"
+            />
+          </Svg>
+        </Back>
+        <Typograph size={12} color={COLORS.GREY_800}>
+          CARRINHO
+        </Typograph>
+        <View />
+      </Container>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/screens/Checkout/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  height: 56px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 20px;
+`;
+
+export const Back = styled.Pressable`
+  padding: 20px 20px 20px 0;
+`;
